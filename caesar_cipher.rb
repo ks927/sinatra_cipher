8b43492d7b27a302bd7843a9d6d4251efcc456cd
KSheehan <ksheehan927@gmail.com>
2017-04-05 11:26:43 -0400

    cipher complete and functional

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,46 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+def caesar_cipher(string, num)
+    @cipher = ""
+
+    string.each_char do |ch|
+        ch = ch.ord
+        if ch.between?(97, 122)  || ch.between?(65, 90)
+            ch += (num % 26)
+        elsif ch > 122 || ch.between?(90, 97)
+            ch -= (num % 26)
+        end
+        ch = ch.chr
+        @cipher << ch
+    end
+    @cipher
+
+end
 
 get '/' do
-erb :index
+string = params['string']
+num = params['num'].to_i
+@cipher = caesar_cipher(string, num)
+erb :index,  :locals => { :cipher => @cipher }
 end
+
+
+=begin
+def caesar_cipher(string, num)
+    cipher = ""
+
+    string.each_char do |ch|
+        ch = ch.ord
+        if ch.between?(97, 122)  || ch.between?(65, 90)
+            ch += (num % 26)
+        elsif ch > 122 || ch.between?(90, 97)
+            ch -= (num % 26)
+        end
+        ch = ch.chr
+        cipher << ch
+    end
+    return cipher
+
+end
+=end
