930959f09714ff52428853b0ae6b33def89968e5
KSheehan <ksheehan927@gmail.com>
2017-04-05 12:18:01 -0400

    add styling

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -24,23 +24,3 @@ num = params['num'].to_i
 @cipher = caesar_cipher(string, num)
 erb :index,  :locals => { :cipher => @cipher }
 end
-
-
-=begin
-def caesar_cipher(string, num)
-    cipher = ""
-
-    string.each_char do |ch|
-        ch = ch.ord
-        if ch.between?(97, 122)  || ch.between?(65, 90)
-            ch += (num % 26)
-        elsif ch > 122 || ch.between?(90, 97)
-            ch -= (num % 26)
-        end
-        ch = ch.chr
-        cipher << ch
-    end
-    return cipher
-
-end
-=end
