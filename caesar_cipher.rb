629673eed4e77fd39c1c3bc357b46452ba7671c1
KSheehan <ksheehan927@gmail.com>
2017-05-12 13:54:57 -0400

    Bug fix- letters wrap from z to a

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,16 +6,24 @@ def caesar_cipher(string, num)
     @cipher = ""
 
     string.each_char do |ch|
-        ch = ch.ord
-        if ch.between?(97, 122)  || ch.between?(65, 90)
-            ch += (num % 26)
-        elsif ch > 122 || ch.between?(90, 97)
-            ch -= (num % 26)
-        end
-        ch = ch.chr
-        @cipher << ch
+      ch = ch.ord
+      if ch.between?(97, 122)
+        shift = ch += (num % 26)
+            if shift > 122 
+                shift = (ch - 123 + 97).chr
+            end
+       elsif ch.between?(65, 90)
+         shift = ch += (num % 26)
+            if shift > 90 
+                shift = (ch - 91 + 65).chr
+            end
+       else
+        shift = ch.chr
+       end
+      
+      @cipher << shift
     end
-    @cipher
+    return @cipher
 
 end
 
